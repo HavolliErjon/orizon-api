32ffa6ca04d4de008237973c2ef2d3bd3ec88064
Erjon Havolli <Havollierjon@hotmail.com>
2025-05-05 00:23:41 +0200

    update

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -39,3 +39,17 @@ exports.updateUser = async (req, res) => {
     res.status(500).json({ message: 'Error updating user', error: error.message });
   }
 };
+
+
+exports.deleteUser = async (req, res) => {
+  try {
+    const { userId } = req.params;
+    const deletedUser = await User.findByIdAndDelete(userId);
+    if (!deletedUser) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+    res.status(200).json({ message: 'User deleted' });
+  } catch (err) {
+    res.status(500).json({ message: 'Error deleting user', error: err.message });
+  }
+};
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -2,8 +2,10 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
 
+
 router.post('/', userController.createUser);
 router.get('/', userController.getUsers);
 router.put('/:userId', userController.updateUser);
+router.delete('/:userId', userController.deleteUser);
 
 module.exports = router;
